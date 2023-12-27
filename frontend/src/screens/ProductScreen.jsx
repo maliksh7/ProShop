0353078a9130855d97d2a5a7a7baaec4bb7bce31
Saad's Macbook Pro <maliksh7@Saadss-MBP.fritz.box>
2023-12-28 00:16:58 +0100

    started integrating backend [GET Req]

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,21 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Card, Col, Image, ListGroup, Row } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
 import Rating from '../components/Rating';
-import products from '../products';
 
 const ProductScreen = () => {
-  const {id: productId} = useParams()
-  const product = products.find(p => p._id === productId)
-  console.log(product)
+  const [product, setProduct] = useState({});
+  const { id: productId } = useParams();
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    }
+    fetchProducts()
+  }, [productId]);
+  
+  
   return (
     <>
       <Link to='/' className='btn btn-light my-3'>Go Back</Link>
